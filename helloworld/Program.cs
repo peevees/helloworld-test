ce7d4b9e8f05bb92a093c937b432104bfa782b98
Mike <peevees@users.noreply.github.com>
2019-08-11 14:22:26 +0200

    Adding scrap for unsuccessful build

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -8,6 +8,7 @@ namespace helloworld
         {
             Console.WriteLine($"Hello World! arguments: {args.Length}");
             throw new Exception("Test Exception");
+            öslajdoijhjwsa
         }
     }
 }
